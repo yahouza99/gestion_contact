a553bdc0a8d3d9cc3061a9a6f73647c233e7bb07
Adamou Yahouza <yahouzaadamou99@gmail.com>
2023-02-26 13:29:02 +0100

    detail

--- app/src/main/java/com/example/contactpro/Detail.java
+++ app/src/main/java/com/example/contactpro/Detail.java
@@ -3,12 +3,29 @@ package com.example.contactpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Detail extends AppCompatActivity {
-
+    TextView nom;
+    TextView prenom;
+    TextView tel;
+    TextView email;
+    TextView service;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        nom= (TextView) findViewById(R.id.nom);
+        prenom= (TextView) findViewById(R.id.prenom);
+        tel= (TextView) findViewById(R.id.tel);
+        email= (TextView) findViewById(R.id.email);
+        service= (TextView) findViewById(R.id.service);
+        Bundle extras=getIntent().getExtras();
+        nom.setText("nom :"+" "+extras.getString("nom"));
+        prenom.setText("prenom :"+" "+extras.getString("prenom"));
+        tel.setText("tel :"+" "+extras.getString("tel"));
+        email.setText("email :"+" "+extras.getString("email"));
+        service.setText("service :"+" "+extras.getString("service"));
     }
 }
--- app/src/main/java/com/example/contactpro/MyAdapter.java
+++ app/src/main/java/com/example/contactpro/MyAdapter.java
@@ -1,18 +1,29 @@
 package com.example.contactpro;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.contactpro.Contact;
 import com.example.contactpro.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -21,7 +32,7 @@ import java.util.LinkedList;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     private  Context context;
     private LinkedList<Contact> contacts;
-  
+
 
     // Provide a suitable constructor (depends on the kind of dataset)
     public MyAdapter(LinkedList<Contact> contacts, Context context) {
@@ -77,8 +88,35 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             itemLayoutView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent Detail= new Intent(v.getContext(),Detail.class);
-                    v.getContext().startActivity(Detail);
+                    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                    FirebaseUser currentUser = mAuth.getCurrentUser();
+                    FirebaseFirestore db=FirebaseFirestore.getInstance();
+                    DocumentReference docRef = db.collection("users").document(currentUser.getEmail());
+                    docRef.collection("contacts").document("7494855").get()
+                            .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                if (document.exists()) {
+                                    Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                                    Intent Detail= new Intent(v.getContext(),Detail.class);
+                                            Detail.putExtra("nom",document.getString("nom"));
+                                            Detail.putExtra("prenom",document.getString("prenom"));
+                                            Detail.putExtra("tel",document.getString("tel"));
+                                            Detail.putExtra("email",document.getString("email"));
+                                            Detail.putExtra("service",document.getString("service"));
+                                    v.getContext().startActivity(Detail);
+                                } else {
+                                    Log.d(TAG, "No such document");
+                                }
+                            } else {
+                                Log.d(TAG, "get failed with ", task.getException());
+                            }
+                        }
+                    });
+
+
                 }
             });
         }
